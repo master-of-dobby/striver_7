1167da9aab05b12546290e2ceec7a5bbd82b97c8
Naveen M <74174115+master-of-dobby@users.noreply.github.com>
2022-09-15 22:55:30 +0530

    Update d_1_8.cpp

--- d_1_8.cpp
+++ d_1_8.cpp
@@ -7,7 +7,7 @@ int main()
     int p=1,n=5;
 
     for(int i=n; i>=1; i--){
-        for(int j=1; j<=p; j++){
+        for(int j=1; j<p; j++){
             cout << " ";
         }
         for(int k=1; k<=(2*i-1); k++)
