2c2dbfd393a7d18f2587cd062ab9215651bd61b8
Naveen M <74174115+master-of-dobby@users.noreply.github.com>
2022-09-16 09:47:33 +0530

    Create d_2_2.cpp

---
+++ d_2_2.cpp
@@ -0,0 +1,20 @@
+//welcome program for striver
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n=5;
+    int k=1;
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++)
+            cout << k++ << " ";
+        
+        cout << endl;
+    }
+
+   
+
+   
+}
