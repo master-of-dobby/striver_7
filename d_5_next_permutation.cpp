81fafffed0f9bdde3bcb60610a08dba2afc92d38
Naveen M <74174115+master-of-dobby@users.noreply.github.com>
2022-09-20 15:56:04 +0530

    Create d_5_next_permutation.cpp

---
+++ d_5_next_permutation.cpp
@@ -0,0 +1,51 @@
+//practice stl - queue
+#include<bits/stdc++.h>
+using namespace std;
+
+void printList(list<int> l){
+    for(auto it = l.begin(); it != l.end(); it++)
+        cout << "->" << *it;
+    
+}
+
+
+
+int main()
+{
+    list<int> l;
+
+    for(int i=1; i<5; i++)
+        l.push_back(i);
+    
+    for(int i=5; i<=10; i++)
+        l.push_front(i);
+    
+    printList(l);
+
+    cout << "\n\tAfter sorting : \n ";
+
+    l.sort();
+
+    printList(l);
+
+    cout << "\n\t After reversing \n ";
+
+    l.reverse();
+
+    printList(l);
+
+    next_permutation(l.begin(), l.end());
+    cout << "\n\t next permutatuion \n";
+
+    printList(l);
+
+    next_permutation(l.begin(), l.end());
+    cout << endl;
+    printList(l);
+
+    
+
+
+    
+
+}
