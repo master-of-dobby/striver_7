e222f7e62928a229e09aeb3b39d486d440a6fd0e
Naveen M <74174115+master-of-dobby@users.noreply.github.com>
2022-09-20 22:11:37 +0530

    Create d_5_print_name_ntimes.cpp

---
+++ d_5_print_name_ntimes.cpp
@@ -0,0 +1,24 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+void fun(int n){
+    if(n == 0)
+        return;
+    
+    cout << "striver" << endl;
+    fun(n-1);
+
+}
+
+
+int main()
+{
+    int n;
+    cout << "Enter the count : ";
+    cin >> n;
+
+    fun(n);
+    
+    return 0;
+}
