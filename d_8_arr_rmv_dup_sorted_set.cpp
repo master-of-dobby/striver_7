b23d85c5371a7a83a77bd6e16ad4a6e0db4ba0f8
Naveen M <74174115+master-of-dobby@users.noreply.github.com>
2022-09-23 12:00:05 +0530

    Create d_8_arr_rmv_dup_sorted_set.cpp

---
+++ d_8_arr_rmv_dup_sorted_set.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:
+    int remove_duplicate(int a[],int n){
+        set<int> s;
+
+        for(int i=0; i<n; i++)
+            s.insert(a[i]);
+        
+        int k = s.size(), i=0;
+
+        for(auto x : s)
+            a[i++] = x;
+        
+        return k;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int N;
+        cin>>N;
+        int a[N];
+        for(int i=0;i<N;i++)
+        {
+            cin>>a[i];
+        }
+    Solution ob;
+    int n = ob.remove_duplicate(a,N);
+
+    for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
+    cout<<endl;
+    }
+}
+// } Driver Code Ends
