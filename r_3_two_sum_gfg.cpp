24bc1a3a11667666e0f84bf68a3b6abefc66b0a4
Naveen M <74174115+master-of-dobby@users.noreply.github.com>
2022-11-02 09:59:34 +0530

    Add files via upload

---
+++ r_3_two_sum_gfg.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    vector<pair<int,int>> allPairs(int a[], int b[], int m, int n, int x)
+    {
+        vector<pair<int, int>> res;
+        // Your code goes here 
+         for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(a[i] + b[j] == x){
+                    res.push_back({a[i], b[j]});
+                }
+            }
+        }
+        
+        sort(res.begin(), res.end());
+
+
+        return res;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+	long long t;
+	cin >> t;
+	
+	while(t--){
+	    int N, M, X;
+	    cin >> N >> M >> X;
+        int A[N], B[M];
+        
+	    for(int i = 0;i<N;i++)
+	        cin >> A[i];
+	    for(int i = 0;i<M;i++)
+	        cin >> B[i];
+	        
+	   Solution ob;
+	   vector<pair<int,int>> vp = ob.allPairs(A, B, N, M, X);
+	   int sz = vp.size();
+        if(sz==0)
+        cout<<-1<<endl;
+        else{
+            for(int i=0;i<sz;i++){
+                if(i==0)
+                cout<<vp[i].first<<" "<<vp[i].second;
+                else
+                cout<<", "<<vp[i].first<<" "<<vp[i].second;
+            }
+            cout<<endl;
+        }
+	}
+	return 0;
+}
+// } Driver Code Ends
