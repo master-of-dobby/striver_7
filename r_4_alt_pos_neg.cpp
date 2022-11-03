fe4e897bbd131cb5fc91e6e3126e309fdf132d6e
Naveen M <74174115+master-of-dobby@users.noreply.github.com>
2022-11-03 23:04:31 +0530

    Add files via upload

---
+++ r_4_alt_pos_neg.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+
+	void rearrange(int arr[], int n) {
+	    // code here
+	    vector<int> pos;
+	    vector<int> neg;
+	    vector<int> res;
+	    
+	    for(int i=0; i<n; i++){
+	        if(arr[i] >= 0)
+	            pos.push_back(arr[i]);
+	       else if(arr[i] < 0)
+	            neg.push_back(arr[i]);
+	       
+	    }
+	    
+	    
+	    
+	    int p_count=pos.size(),n_count=neg.size();
+	    //cout << p_count << " " << n_count;
+	    
+	    
+	    for(int i=0; i<n; i++){
+	        if(p_count){
+	            res.push_back(pos[i]);
+	            p_count--;
+	            //i++;
+	        }
+	        if(n_count){
+	            res.push_back(neg[i]);
+	            n_count--;
+	        }
+	        
+	        
+ 	    }
+ 	    
+ 	    //for(int i=0; i<res.size(); i++)
+ 	      //    cout << res[i] << " ";
+ 	    
+ 	    for(int i=0; i<n; i++)
+ 	        arr[i] = res[i];
+ 	    
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.rearrange(arr, n);
+        for (i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
