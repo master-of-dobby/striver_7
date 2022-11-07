86c5a87e09abcce353bb7984933953d53c7b4974
Naveen M <74174115+master-of-dobby@users.noreply.github.com>
2022-11-07 23:25:07 +0530

    Add files via upload

---
+++ r_5_next_permutations.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+    vector<int>a = {1, 3, 5, 4, 2};
+	int n = a.size(), k, l;
+
+	for(k = n-2; k >=0; k--){
+		if(a[k] < a[k+1])
+			break;
+	}
+	if(k < 0){
+		reverse(a.begin(), a.end());
+	}
+	else{
+		for(l = n-1; l > k; l--){
+			if(a[l] > a[k])
+				break;
+		}
+		
+		swap(a[k], a[l]);
+
+		reverse(a.begin()+k+1, a.end());
+	}
+
+	
+
+	for(int i=0; i<a.size(); i++)
+		cout << a[i] << " ";
+
+
+
+
+	//next_permutation(arr, arr+3);
+   
+    
+    return 0;
+}
+
